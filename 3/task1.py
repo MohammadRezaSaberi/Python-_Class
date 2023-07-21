89637da1a4097a21ebae5509f7c9c30ffc16104b
Mohammad Reza Saberi <139690792+MohammadRezaSaberi@users.noreply.github.com>
2023-07-21 20:20:33 +0330

    Add files via upload

---
+++ 3/task1.py
@@ -0,0 +1,8 @@
+a = float(input('a: '))
+b = float(input('b: '))
+
+s = a + b
+m = a - b
+
+print('sum a, b =' , s)
+print('min a, b =' , m)
---
+++ 3/task2.py
@@ -0,0 +1,7 @@
+n = float(input('please inter the year of your birth: '))
+
+m = float(input('please inter the current year: '))
+
+age = m - n
+
+print('age = ', age)
